55bdf7f60dfee0ab9d6e88c3166460db4820b514
xue.zhang <angelina.x.zhang@pwc.com>
2017-07-26 17:54:37 +0800

    add effect

--- app/modules/modules.js
+++ app/modules/modules.js
@@ -2,8 +2,12 @@ import test from './example/app.vue';
 import appHeader from './header/header.component.vue';
 import appSidebar from './sidebar/sidebar.component.vue';
 
+// recipe components
+import pointerHoverEffects from './pointerHoverEffects/pointerHoverEffects.component.vue';
+
 export default {
   test,
   appHeader,
-  appSidebar
+  appSidebar,
+  pointerHoverEffects
 };
---
+++ app/modules/pointerHoverEffects/pointerHoverEffects.component.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="page pointer-hover-effects">
+    <div class="page__demo">
+      <div class="page__container">
+        <div class="photobox photobox_type1">
+          <div class="photobox__previewbox">
+            <div class="photobox__preview"></div>
+            <span class="photobox__label">Effect #1</span>
+          </div>
+        </div>
+        <div class="photobox photobox_type2">
+          <div class="photobox__previewbox">
+            <div class="photobox__preview"></div>
+            <span class="photobox__label">Effect #2</span>
+          </div>
+        </div>
+        <div class="photobox photobox_type3">
+          <div class="photobox__previewbox">
+            <div class="photobox__preview"></div>
+            <span class="photobox__label">Effect #3</span>
+          </div>
+        </div>
+        <div class="photobox photobox_type4">
+          <div class="photobox__previewbox">
+            <div class="photobox__preview"></div>
+            <span class="photobox__label">Effect #4</span>
+          </div>
+        </div>
+        <div class="photobox photobox_type5">
+          <div class="photobox__previewbox">
+            <div class="photobox__preview"></div>
+            <span class="photobox__label">Effect #5</span>
+          </div>
+        </div>
+        <div class="photobox photobox_type6">
+          <div class="photobox__previewbox">
+            <div class="photobox__preview"></div>
+            <span class="photobox__label">Effect #6</span>
+          </div>
+        </div>
+        <div class="photobox photobox_type7">
+          <div class="photobox__previewbox">
+            <div class="photobox__preview"></div>
+            <span class="photobox__label">Effect #7</span>
+          </div>
+        </div>
+        <div class="photobox photobox_type8">
+          <div class="photobox__previewbox">
+            <div class="photobox__preview"></div>
+            <span class="photobox__label">Effect #8</span>
+          </div>
+        </div>
+        <div class="photobox photobox_type9">
+          <div class="photobox__previewbox">
+            <div class="photobox__preview"></div>
+            <span class="photobox__label">Effect #9</span>
+          </div>
+        </div>
+        <div class="photobox photobox_type10">
+          <div class="photobox__previewbox">
+            <div class="photobox__preview"></div>
+            <span class="photobox__label">Effect #10</span>
+          </div>
+        </div>
+        <div class="photobox photobox_type11">
+          <div class="photobox__previewbox">
+            <div class="photobox__preview"></div>
+            <span class="photobox__label">Effect #11</span>
+          </div>
+        </div>
+        <div class="photobox photobox_type12">
+          <div class="photobox__previewbox">
+            <div class="photobox__preview"></div>
+            <span class="photobox__label">Effect #12</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  title: 'Pointer Hover Effects'
+}
+</script>
