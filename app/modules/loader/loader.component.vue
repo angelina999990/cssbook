2f4b712242f6b09bb3de5d4e2d43dfcae54dc42d
angelina999990 <angelina999990@gmail.com>
2017-07-30 17:10:54 +0800

    recipe 2 ing

---
+++ app/modules/loader/loader.component.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="wrapper">
+    <div class="preview-box">
+      <div class="loader-1"></div>
+    </div>
+    <div class="preview-box">
+      <div class="loader-2">
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  title: 'Loader'
+}
+</script>
--- app/modules/modules.js
+++ app/modules/modules.js
@@ -4,10 +4,13 @@ import appSidebar from './sidebar/sidebar.component.vue';
 
 // recipe components
 import pointerHoverEffects from './pointerHoverEffects/pointerHoverEffects.component.vue';
+import loader from './loader/loader.component.vue';
 
 export default {
   home,
   appHeader,
   appSidebar,
-  pointerHoverEffects
+
+  pointerHoverEffects,
+  loader
 };
