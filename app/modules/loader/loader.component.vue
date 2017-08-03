c42a4fc44ccca82e19657869a5ce76d115786fb0
xue.zhang <angelina.x.zhang@pwc.com>
2017-08-03 14:57:15 +0800

    recipe - loader - add one

--- app/modules/loader/loader.component.vue
+++ app/modules/loader/loader.component.vue
@@ -4,8 +4,14 @@
       <div class="loader-1"></div>
     </div>
     <div class="preview-box">
-      <div class="loader-2">
-        
+      <div class="loader-2"></div>
+    </div>
+    <div class="preview-box">
+      <div class="loader-3">
+        <span class="progress-bar"></span>
+        <span class="progress-bar"></span>
+        <span class="progress-bar"></span>
+        <span class="progress-bar"></span>
       </div>
     </div>
   </div>
